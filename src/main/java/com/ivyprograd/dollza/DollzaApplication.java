a69d0c93cd54dfe1d731368ed4c085d5ee8938a3
saishma4 <chanda.saishma@ivycomptech.com>
2022-09-30 15:32:06 +0530

    initial commit

---
+++ src/main/java/com/ivyprograd/dollza/DollzaApplication.java
@@ -0,0 +1,13 @@
+package com.ivyprograd.dollza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DollzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivyprograd/dollza/DollzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivyprograd.dollza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DollzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
