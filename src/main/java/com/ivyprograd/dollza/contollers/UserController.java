2293de18769c18945febe5cf2f655c5945f997da
saishma4 <chanda.saishma@ivycomptech.com>
2022-09-30 16:10:39 +0530

    user controller initial commit

--- src/main/java/com/ivyprograd/dollza/contollers/UserController.java
+++ src/main/java/com/ivyprograd/dollza/contollers/UserController.java
@@ -1,2 +1,16 @@
-package com.ivyprograd.dollza.contollers;public class UserController {
+package com.ivyprograd.dollza.contollers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class UserController {
+
+    @GetMapping("/all")
+    public void allAccess() {
+        System.out.println("");
+    }
+
 }
