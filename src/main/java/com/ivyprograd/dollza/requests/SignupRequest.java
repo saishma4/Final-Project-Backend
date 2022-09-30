bfb22e82edee75c0af97fc7d573ab21439e22c08
saishma4 <chanda.saishma@ivycomptech.com>
2022-09-30 16:24:57 +0530

    username,email,password,confirmpassword variables commit

--- src/main/java/com/ivyprograd/dollza/requests/SignupRequest.java
+++ src/main/java/com/ivyprograd/dollza/requests/SignupRequest.java
@@ -1,2 +1,13 @@
-package com.ivyprograd.dollza.requests;public class SignupRequest {
+package com.ivyprograd.dollza.requests;
+
+import lombok.Data;
+
+@Data
+public class SignupRequest {
+
+    private String username;
+    private String email;
+    private String password;
+    private String confirmpassword;
+
 }
