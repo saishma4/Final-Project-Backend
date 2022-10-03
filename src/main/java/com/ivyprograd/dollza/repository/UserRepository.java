8e6da245cf31fc1c7e1dea85241516895f6e77d9
saishma4 <chanda.saishma@ivycomptech.com>
2022-10-03 14:46:34 +0530

    user jpa repository commit

--- src/main/java/com/ivyprograd/dollza/repository/UserRepository.java
+++ src/main/java/com/ivyprograd/dollza/repository/UserRepository.java
@@ -4,6 +4,12 @@ package com.ivyprograd.dollza.repository;
 import com.ivyprograd.dollza.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
 
