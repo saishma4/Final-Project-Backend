f568d2d0a93e60b1c2fac24f287c1b792ebd4c73
saishma4 <chanda.saishma@ivycomptech.com>
2022-10-03 14:45:20 +0530

    user details service commit

---
+++ src/main/java/com/ivyprograd/dollza/services/UserDetailsServiceDollza.java
@@ -0,0 +1,29 @@
+package com.ivyprograd.dollza.services;
+
+
+import com.ivyprograd.dollza.models.User;
+import com.ivyprograd.dollza.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+
+@Service
+public class UserDetailsServiceDollza implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("user Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+
+}
