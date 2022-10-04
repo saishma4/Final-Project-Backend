afc160cfa44dda3ebb283f6f46059acfbc669bf0
saishma4 <chanda.saishma@ivycomptech.com>
2022-10-04 08:30:19 +0530

    product service commit

--- src/main/java/com/ivyprograd/dollza/services/ProductDollService.java
+++ src/main/java/com/ivyprograd/dollza/services/ProductDollService.java
@@ -1,2 +1,23 @@
-package com.ivyprograd.dollza.services;public class ProductDollService {
+package com.ivyprograd.dollza.services;
+
+import com.ivyprograd.dollza.models.Dolls;
+import com.ivyprograd.dollza.models.User;
+import com.ivyprograd.dollza.repository.ProductDollRepository;
+import com.ivyprograd.dollza.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ProductDollService {
+    @Autowired
+    private ProductDollRepository productDollRepository;
+
+    public List<Dolls> getDollsList() {
+        return productDollRepository.findAll();
+    }
 }
