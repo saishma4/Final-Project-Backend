7b1da7f74d53125ee20a82f7b7ab730192fd0263
saishma4 <chanda.saishma@ivycomptech.com>
2022-10-04 08:29:40 +0530

    product jpa repository commit

--- src/main/java/com/ivyprograd/dollza/repository/ProductDollRepository.java
+++ src/main/java/com/ivyprograd/dollza/repository/ProductDollRepository.java
@@ -1,2 +1,10 @@
-package com.ivyprograd.dollza.repository;public interface ProductDollRepository {
+package com.ivyprograd.dollza.repository;
+
+import com.ivyprograd.dollza.models.Dolls;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductDollRepository extends JpaRepository<Dolls,String> {
+    Optional<Dolls> findByDollname(String dollname);
 }
