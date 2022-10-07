421a394ad272e7719716452d46037e79e17b93ab
saishma4 <chanda.saishma@ivycomptech.com>
2022-10-07 10:59:48 +0530

    product repository autowire  commit

--- src/main/java/com/ivyprograd/dollza/services/ProductDollService.java
+++ src/main/java/com/ivyprograd/dollza/services/ProductDollService.java
@@ -1,14 +1,9 @@
 package com.ivyprograd.dollza.services;
 
 import com.ivyprograd.dollza.models.Dolls;
-import com.ivyprograd.dollza.models.User;
 import com.ivyprograd.dollza.repository.ProductDollRepository;
-import com.ivyprograd.dollza.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,6 +11,10 @@ import java.util.List;
 public class ProductDollService {
     @Autowired
     private ProductDollRepository productDollRepository;
+
+    public ProductDollService(ProductDollRepository productDollRepository) {
+        this.productDollRepository = productDollRepository;
+    }
 
     public List<Dolls> getDollsList() {
         return productDollRepository.findAll();
